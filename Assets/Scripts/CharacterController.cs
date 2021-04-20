fe450436b2a840299b4aeaafd2ab0a7238635548
Nixon Silva <nixonmoreira42@gmail.com>
2021-04-20 14:32:09 -0300

    Day 1 Class
    Implemented Tilemaps into the default scene, added the player character and implemented horizontal movement with smooth dampening;

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    // Health e speed serão variáveis comuns para ambos o jogador e os inimigos
+    [SerializeField] private float health = 100f;
+    [SerializeField] private float speed = 100f;
+
+    // Precisamos do rigidbody para computar os movimentos
+    private Rigidbody2D rb;
+
+    // Verifica se o sprite está olhando para a direita ou não
+    private bool isFacingRight = true;
+
+    // Verifica se o player pode mexer ou não
+    private bool canMove = true;
+
+    // Velocidade atual do objeto usado para cálculos
+    private float currentSpeed = 0f;
+
+    // Tempo para suavizar o movimento
+    [SerializeField] private float dampeningTime = 0.15f;
+
+    // Awake é executado antes do Start
+    private void Awake ()
+    {
+        rb = GetComponent<Rigidbody2D>();        
+    }
+
+    // Update é chamado uma vez por frame
+    private void Update()
+    {
+        
+    }
+
+    protected void PerformMove (Vector2 movement, bool canNormalize)
+    {
+        // Realiza as operações de movimento
+        // com base no motor físico
+        if (canNormalize)
+        {
+            movement.Normalize();
+        }
+
+        if (canMove)
+        {
+            AdjustOrientation(movement);
+            rb.AddForce(movement * Time.fixedDeltaTime * speed, ForceMode2D.Impulse);
+            // rb.MovePosition(rb.position + movement * Time.fixedDeltaTime * speed);
+        }
+
+    }
+
+    private void AdjustOrientation (Vector2 movement)
+    {
+        // Verifica a direção do movimento e 
+        // a qual direção o sprite está olhando
+        if (isFacingRight && movement.x < 0)
+        {
+            isFacingRight = false;
+            Flip();
+            
+        }
+
+        if (!isFacingRight && movement.x > 0)
+        {
+            isFacingRight = true;
+            Flip();
+        }
+    }
+
+    private void Flip ()
+    {
+        // Muda o sprite de direção dependendo do movimento
+        Vector3 objectScale = transform.localScale;
+        objectScale.x *= -1;
+        transform.localScale = objectScale;
+    }
+
+    protected void StopBody ()
+    {
+        // Queremos manter a velocidade em y por conta da gravidade
+        // SmoothDamp suaviza o movimento com base em dampeningTime
+        Vector2 dampenedVelocity = new Vector2(Mathf.SmoothDamp(rb.velocity.x, 0f, ref currentSpeed, dampeningTime), rb.velocity.y);
+        rb.velocity = dampenedVelocity;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : CharacterController
+{
+
+    private float horizontalInput;
+
+    // FixedUpdate é chamado uma quantidade fixa por segundos
+    private void FixedUpdate()
+    {
+        // Gerencia o input horizontal
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        // Realiza o movimento caso o input seja diferente de zero
+        // i.e. há input
+        if (!Mathf.Approximately(horizontalInput, 0f))
+        {
+            PerformMove(new Vector2(horizontalInput, 0f), false);
+        }
+        // Caso contrário, pare o objeto para ele não deslizar
+        else
+        {
+            StopBody();
+        }
+
+    }
+}
